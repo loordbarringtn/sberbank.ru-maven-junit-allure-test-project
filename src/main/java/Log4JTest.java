716847fd607ed29b385ae74cdb4c96073f615601
loordbarringtn <X_ernya123>
2020-05-14 17:02:54 +0300

    Log4j2  framework added

---
+++ src/main/java/Log4JTest.java
@@ -0,0 +1,16 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4JTest {
+    private static final Logger logger = LogManager.getLogger(Log4JTest.class);
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello, user!" );
+        logger.trace("We've just greeted the user!");
+        logger.debug("We've just greeted the user!");
+        logger.info("We've just greeted the user!");
+        logger.warn("We've just greeted the user!");
+        logger.error("We've just greeted the user!");
+        logger.fatal("We've just greeted the user!");
+    }
+}
