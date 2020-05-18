87891e17cd9ec7815e2becde030aed3805d53c68
loordbarringtn <X_ernya123>
2020-05-18 22:59:45 +0300

    All tests are created!

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,6 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,6 +25,20 @@ public class MainTest {
     }
 
     @Test
+    @DisplayName("Тестирование входа в Сбербанк Онлайн Демо")
+    public void findAndLoginDemoCabinet (){
+        open("https://www.sberbank.ru/ru/");
+        $(By.cssSelector(".ya-site-form-search__button")).click();
+        $(By.className("ya-site-form__input-text")).setValue("демо").pressEnter();
+        $(By.xpath("(//yass-span[contains(.,'Демо-версия Сбербанк Онлайн')])[1]")).click();
+        switchTo().window(0).close();
+        switchTo().window(0);
+        $(By.xpath("//a[@class='close']")).click();
+        String textToCheck = $(By.xpath("//div[@class='simulator-title']")).getText();
+        assertTrue(textToCheck.contains("Демо-версия"));
+    }
+
+    @Test
     @DisplayName("Тестирование входа в Сбербанк Бизнес Онлайн Демо")
     public void findAndLoginBusinessDemoCabinet (){
         open("https://www.sberbank.ru/ru/");
@@ -38,6 +51,29 @@ public class MainTest {
         $(By.xpath("//a[@href='https://sbi.sberbank.ru:9444/ic/dcb/login.html?demoMode&version=3.0']")).click();
         String textToCheck = $(by("data-test-id", "HeaderProfileWidget__userName")).getText();
         assertTrue(textToCheck.contains("Иванов Иван Иванович"));
+    }
+
+    @Test
+    @DisplayName("Тестирование раздела Вакансии")
+    public void searchJob (){
+        open("https://www.sberbank.ru/ru/");
+        Selenide.executeJavaScript("window.scrollBy(0,10000)");
+        $(By.xpath("//a[contains(@class,'link_s')][contains(text(),'Вакансии')]")).click();
+        $(By.className("career__search-input")).setValue("Java").pressEnter();
+        switchTo().window(0).close();
+        switchTo().window(0);
+        $(By.xpath("//a[contains(text(),'Java разработчик')]")).waitUntil(Condition.visible, 60000).click();
+    }
+
+    @Test
+    @DisplayName("Тестирование раздела о Банке и проставление оценки")
+    public void rateBank (){
+        open("https://www.sberbank.ru/ru/");
+        $(By.xpath("//a[contains(text(),'О банке')]")).click();
+        $(By.xpath("//a[contains(.,'О миссии и ценностях банка')]")).click();
+        $(By.xpath("//i[contains(@class,'like-form__icon-like')]//*[local-name()='svg']")).click();
+        $(By.xpath("//textarea[@placeholder='Что мы могли бы улучшить?']")).setValue("Много чего!");
+        $(By.xpath("//button[@class='kit-button kit-button_type_big like-form__button']")).click();
     }
 
 }
