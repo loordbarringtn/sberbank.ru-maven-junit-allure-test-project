87b19bdc43b25ce5fa85c2c70b4dd6971e33bcd8
loordbarringtn <X_ernya123>
2020-05-20 22:06:05 +0300

    All tests refactored in Allure style

--- src/test/java/CoreLogic/AllureSteps.java
+++ src/test/java/CoreLogic/AllureSteps.java
@@ -1,6 +1,11 @@
 package CoreLogic;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -27,9 +32,24 @@ public class AllureSteps {
         $(By.className("ya-site-form__input-text")).setValue(word).pressEnter();
     }
 
-    @Step("Click on SBOL Demo")
-    public void clickOnSBOLDemo(){
-        $(By.xpath("(//yass-span[contains(.,'Демо-версия Сбербанк Онлайн')])[1]")).click();
+    @Step("Click on search results: {searchResults}")
+    public void clickOnSearchResults(String searchResults){
+        $(By.xpath("(//yass-span[contains(.,'"+searchResults+"')])[1]")).click();
+    }
+
+    @Step("Click on web element: {webElement}")
+    public void clickOnWebElement(String webElement){
+        $$(byText(""+webElement+"")).find(Condition.visible).click();
+    }
+
+    @Step("Click on Demo mode to continue")
+    public void clickOnDemoMode(){
+        $(By.xpath("//a[@href='https://sbi.sberbank.ru:9444/ic/dcb/login.html?demoMode&version=3.0']")).click();
+    }
+
+    @Step("Searching Vacancy: {webElement}")
+    public void searchVacancy(String vacancyName){
+        $(By.className("career__search-input")).setValue(""+vacancyName+"").pressEnter();
     }
 
     @Step("Close on needless tab")
@@ -38,16 +58,36 @@ public class AllureSteps {
         switchTo().window(0);
     }
 
+    @Step("Scroll to the Bottom of the Page")
+    public void scrollToBottomOfPage(){
+        Selenide.executeJavaScript("window.scrollBy(0,10000)");
+    }
+
     @Step("Close popup")
     public void closePopUp(){
         $(By.xpath("//a[@class='close']")).click();
     }
 
-    @Step("Check that we logged in successfully")
+    @Step("Click on like")
+    public void clickOnLike(){
+        $(By.xpath("//i[contains(@class,'like-form__icon-like')]//*[local-name()='svg']")).click();
+    }
+
+    @Step("Type text in opinion review")
+    public void typeOpinionText(){
+        $(By.xpath("//textarea[@placeholder='Что мы могли бы улучшить?']")).setValue("Много чего!");
+    }
+
+    @Step("Check that we logged in demo SBOL successfully")
     public void checkSuccessSBOLLogin(){
         String textToCheck = $(By.xpath("//div[@class='simulator-title']")).getText();
         assertTrue(textToCheck.contains("Демо-версия"));
     }
 
+    @Step("Check that we logged in demo Sberbank Business online successfully")
+    public void checkSuccessBusinessOnlineLogin(){
+        String textToCheck = $(by("data-test-id", "HeaderProfileWidget__userName")).getText();
+        assertTrue(textToCheck.contains("Иванов Иван Иванович"));
+    }
 
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,5 @@
 import CoreLogic.AllureSteps;
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -9,11 +7,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest {
     private AllureSteps steps = new AllureSteps();
@@ -34,6 +28,7 @@ public class MainTest {
     @Test
     @DisplayName("Тестирование главной страницы")
     @Description(value = "Загрузка страницы и проверка текста с лицензией")
+    @Epic("Регресс тесты")
     @Severity(value = SeverityLevel.BLOCKER)
     public void openWebsite (){
         steps.openWebsite();
@@ -50,7 +45,7 @@ public class MainTest {
         steps.openWebsite();
         steps.clickOnSearchCircle();
         steps.typeWordToSearch("Демо-версия Сбербанк Онлайн");
-        steps.clickOnSBOLDemo();
+        steps.clickOnSearchResults("Демо-версия Сбербанк Онлайн");
         steps.closeNeedlessTab();
         steps.closePopUp();
         steps.checkSuccessSBOLLogin();
@@ -58,41 +53,50 @@ public class MainTest {
 
     @Test
     @DisplayName("Тестирование входа в Сбербанк Бизнес Онлайн Демо")
+    @Description(value = "Пытаемся войти в демо кабинет Сбербанк Бизнес онлайн")
+    @Epic("Регресс тесты")
+    @Feature("Тестирование онлайн кабинетов")
+    @Severity(value = SeverityLevel.CRITICAL)
     public void findAndLoginBusinessDemoCabinet (){
-        open("https://www.sberbank.ru/ru/");
-        $(By.xpath("//div[contains(@class, 'search__button')]")).click();
-        $(By.className("ya-site-form__input-text")).setValue("Бизнес онлайн").pressEnter();
-        $(By.xpath("//yass-span[contains(.,'(СББОЛ)')]")).click();
-        switchTo().window(0).close();
-        switchTo().window(0);
-        $(By.xpath("//a[contains(@class,'b-btn btn-inverse b-btn_green-hover')]")).click();
-        $(By.xpath("//a[@href='https://sbi.sberbank.ru:9444/ic/dcb/login.html?demoMode&version=3.0']")).click();
-        String textToCheck = $(by("data-test-id", "HeaderProfileWidget__userName")).getText();
-        assertTrue(textToCheck.contains("Иванов Иван Иванович"));
+        steps.openWebsite();
+        steps.clickOnSearchCircle();
+        steps.typeWordToSearch("Бизнес онлайн");
+        steps.clickOnSearchResults("СББОЛ");
+        steps.closeNeedlessTab();
+        steps.clickOnWebElement("Демо-доступ");
+        steps.clickOnDemoMode();
+        steps.checkSuccessBusinessOnlineLogin();
     }
 
     @Test
+    @Flaky
+    @Issue("Время от времени, поиск выдаёт значение, отличное от значения, введённого в строку поиска")
     @DisplayName("Тестирование раздела Вакансии")
-    public void searchJob (){
-        open("https://www.sberbank.ru/ru/");
-        Selenide.executeJavaScript("window.scrollBy(0,10000)");
-        $(By.xpath("//a[contains(@class,'link_s')][contains(text(),'Вакансии')]")).click();
-        $(By.className("career__search-input")).setValue("Java").pressEnter();
-        switchTo().window(0).close();
-        switchTo().window(0);
-        $(By.xpath("//a[contains(text(),'Java разработчик')]")).waitUntil(Condition.visible, 60000).click();
+    @Description(value = "Ищем вакансию для Java разработчика")
+    @Epic("Регресс тесты")
+    @Severity(value = SeverityLevel.NORMAL)
+    public void searchVacancy (){
+        steps.openWebsite();
+        steps.scrollToBottomOfPage();
+        steps.clickOnWebElement("Вакансии");
+        steps.searchVacancy("Java");
+        steps.closeNeedlessTab();
+        steps.clickOnWebElement("Java разработчик");
     }
 
     @Test
     @DisplayName("Тестирование раздела о Банке и проставление оценки")
+    @Description(value = "Пишем текст для оценки страницы")
+    @Epic("Регресс тесты")
+    @Severity(value = SeverityLevel.NORMAL)
     public void rateBank (){
-        open("https://www.sberbank.ru/ru/");
-        $(byText("О банке")).click();
-        $(byText("О миссии и ценностях банка")).click();
-        Selenide.executeJavaScript("window.scrollBy(0,10000)");
-        $(By.xpath("//i[contains(@class,'like-form__icon-like')]//*[local-name()='svg']")).click();
-        $(By.xpath("//textarea[@placeholder='Что мы могли бы улучшить?']")).setValue("Много чего!");
-        $(By.xpath("//button[@class='kit-button kit-button_type_big like-form__button']")).click();
+        steps.openWebsite();
+        steps.clickOnWebElement("О банке");
+        steps.clickOnWebElement("О миссии и ценностях банка");
+        steps.scrollToBottomOfPage();
+        steps.clickOnLike();
+        steps.typeOpinionText();
+        steps.clickOnWebElement("Отправить");
     }
 
 }
