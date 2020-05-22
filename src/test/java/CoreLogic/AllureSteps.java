b706015c1390bc7b114ffd3df14fc2891343bd02
loordbarringtn <X_ernya123>
2020-05-22 13:43:22 +0300

    Steps description written in Russian language

--- src/test/java/CoreLogic/AllureSteps.java
+++ src/test/java/CoreLogic/AllureSteps.java
@@ -3,7 +3,6 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -11,80 +10,80 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AllureSteps {
 
-    @Step("Go to Sberbank website")
+    @Step("Заходим на главную страницу сайта")
     public void openWebsite(){
         open("https://www.sberbank.ru/ru/");
     }
 
-    @Step("Check License Text on Main Page")
+    @Step("Проверяем наличие текста с номером лицензии на главной странице")
     public void checkLicenseTextOnMainPage() {
         String availableText = $(By.xpath("//div[@class='footer__info']//p[contains(text(),'лицензия')]")).getText();
         assertTrue(availableText.contains("Генеральная лицензия на осуществление банковских операций от 11 августа 2015 года. Регистрационный номер — 1481."));
     }
 
-    @Step("Click on search circle")
+    @Step("Кликаем на элемент с изображением лупы")
     public void clickOnSearchCircle(){
         $(By.xpath("//div[@class='ya-site-form-search__button ya-site-form-search_visible ']")).click();
     }
 
-    @Step("Type word: {word} to search")
+    @Step("Вводим слово: {word} для поиска")
     public void typeWordToSearch(String word){
         $(By.className("ya-site-form__input-text")).setValue(word).pressEnter();
     }
 
-    @Step("Click on search results: {searchResults}")
+    @Step("Кликаем на результат поиска: {searchResults}")
     public void clickOnSearchResults(String searchResults){
         $(By.xpath("(//yass-span[contains(.,'"+searchResults+"')])[1]")).click();
     }
 
-    @Step("Click on web element: {webElement}")
+    @Step("Кликаем по web элементу: {webElement}")
     public void clickOnWebElement(String webElement){
         $$(byText(""+webElement+"")).find(Condition.visible).click();
     }
 
-    @Step("Click on Demo mode to continue")
+    @Step("Кликаем по ссылке для продолжения работы в демо-режиме")
     public void clickOnDemoMode(){
         $(By.xpath("//a[@href='https://sbi.sberbank.ru:9444/ic/dcb/login.html?demoMode&version=3.0']")).click();
     }
 
-    @Step("Searching Vacancy: {webElement}")
+    @Step("Ищем вакансию: {vacancyName}")
     public void searchVacancy(String vacancyName){
         $(By.className("career__search-input")).setValue(""+vacancyName+"").pressEnter();
     }
 
-    @Step("Close on needless tab")
+    @Step("Закрываем ненужную вкладку браузера")
     public void closeNeedlessTab(){
         switchTo().window(0).close();
         switchTo().window(0);
     }
 
-    @Step("Scroll to the Bottom of the Page")
+    @Step("Скролим страницу вниз")
     public void scrollToBottomOfPage(){
         Selenide.executeJavaScript("window.scrollBy(0,10000)");
     }
 
-    @Step("Close popup")
+    @Step("Закрываем всплывающее окно")
     public void closePopUp(){
         $(By.xpath("//a[@class='close']")).click();
     }
 
-    @Step("Click on like")
+    @Step("Кликаем на лайк")
     public void clickOnLike(){
         $(By.xpath("//i[contains(@class,'like-form__icon-like')]//*[local-name()='svg']")).click();
     }
 
-    @Step("Type text in opinion review")
+    @Step("Пишем текст для оценки улучшения страницы")
     public void typeOpinionText(){
         $(By.xpath("//textarea[@placeholder='Что мы могли бы улучшить?']")).setValue("Много чего!");
     }
 
-    @Step("Check that we logged in demo SBOL successfully")
+    @Step("Проверяем что залогинились в Демо версию кабинета Сбербанк онлайн")
     public void checkSuccessSbolLogin(){
         String textToCheck = $(By.xpath("//div[@class='simulator-title']")).getText();
         assertTrue(textToCheck.contains("Демо-версия"));
     }
 
-    @Step("Check that we logged in demo Sberbank Business online successfully")
+    @Step("Проверяем что залогинились в Демо версию кабинета Бизнес Сбербанк онлайн")
     public void checkSuccessBusinessOnlineLogin(){
         String textToCheck = $(by("data-test-id", "HeaderProfileWidget__userName")).getText();
         assertTrue(textToCheck.contains("Иванов Иван Иванович"));
